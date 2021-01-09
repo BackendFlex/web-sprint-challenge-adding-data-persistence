aa78ae8b7a56a42d92fd8a0c2fca41becda9ac7e
Dodger-42 <kevin.bing.barber@gmail.com>
2021-01-08 21:05:37 -0800

    config, index, router, migration

--- api/task/router.js
+++ api/task/router.js
@@ -1,26 +1,26 @@
 // build your `/api/tasks` router here
 
-// const express = require("express");
+const express = require("express");
 
-// const Task = require("./project-model");
+const Task = require("./project-model");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get("/:id/task", (req, res) => {
-//   const { id } = req.params;
-//   Task.getResourceByTaskId(id)
-//     .then((resource) => {
-//       if (resource.length) {
-//         res.json({ resource });
-//       } else {
-//         res
-//           .status(404)
-//           .json({ message: "No resources found for the given task" });
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ message: "Failed to get resources" });
-//     });
-// });
+router.get("/:id/task", (req, res) => {
+  const { id } = req.params;
+  Task.getResourceByTaskId(id)
+    .then((resource) => {
+      if (resource.length) {
+        res.json({ resource });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No resources found for the given task" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get resources" });
+    });
+});
 
-// module.exports = router;
+module.exports = router;
